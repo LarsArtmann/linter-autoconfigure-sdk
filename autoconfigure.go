0cc2c2e3692693706ea251e8afc1e04ae2a6c38f
Lars Artmann <git@lars.software>
2026-09-11 10:14:31 +0200

    chore: auto-commit 4 changed file(s) (heuristic)

--- autoconfigure.go
+++ autoconfigure.go
@@ -209,7 +209,7 @@ func FindingFromIssue(toolName finding.ToolName, issue ConfigIssue) (finding.Fin
 
 	built, err := builder.Build()
 	if err != nil {
-		return finding.Finding{}, fmt.Errorf("autoconfigure: build finding for rule %q: %w", issue.Rule, err)
+		return finding.Finding{}, fmt.Errorf("autoconfigure: build finding for tool %q rule %q: %w", toolName, issue.Rule, err)
 	}
 
 	return built, nil
